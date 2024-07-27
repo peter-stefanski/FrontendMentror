4f67b5d138028cff6b6605a5009b30a0a8f4c162
PIotr Stefanski <peterstefanski3@gmail.com>
2024-07-27 23:34:42 +0200

    change method in html and add js

---
+++ FAQ Accordion Main/faq-accordion-main/index.js
@@ -0,0 +1,9 @@
+const toggleElement = document.querySelector(".possision_card");
+
+details.addEventListener("toggle", (event) => {
+  if (details.open) {
+    /* the element was toggled open */
+  } else {
+    /* the element was toggled closed */
+  }
+});
