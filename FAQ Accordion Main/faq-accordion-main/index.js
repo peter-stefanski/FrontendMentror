7de950ce91c99f5560a18ce6d5d18461e2561092
PIotr Stefanski <peterstefanski3@gmail.com>
2024-07-28 15:16:49 +0200

    finished a desktop project without JS

--- FAQ Accordion Main/faq-accordion-main/index.js
+++
@@ -1,9 +0,0 @@
-const toggleElement = document.querySelector(".possision_card");
-
-details.addEventListener("toggle", (event) => {
-  if (details.open) {
-    /* the element was toggled open */
-  } else {
-    /* the element was toggled closed */
-  }
-});
