33675ef4b1925a0f00510ac8a917271191c11e80
PIotr Stefanski <peterstefanski3@gmail.com>
2024-08-08 23:53:14 +0200

    I've changed name of class but I'm affraid it is wrong but this is realy smal change

--- Article Preview Component/article-preview-component-master/index.js
+++ Article Preview Component/article-preview-component-master/index.js
@@ -1,8 +1,10 @@
-document.getElementById("button").addEventListener("click", function () {
-  document.querySelector(".share").getElementsByClassName.display = "flex";
-  document.getElementById("buttonClose").style.display = "inline-block";
-  document.getElementById("button").style.display = "none";
-});
+document
+  .getElementsByClassName(".button_01")
+  .addEventListener("click", function () {
+    document.querySelector(".share").getElementsByClassName.display = "flex";
+    document.getElementById("buttonClose").style.display = "inline-block";
+    document.getElementById(".button_01").style.display = "none";
+  });
 
 function init() {
   document.querySelector(".share").style.display = "flex";
