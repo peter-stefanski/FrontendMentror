0405163124dccd771bfc36ced36ba0591cc43e04
PIotr Stefanski <peterstefanski3@gmail.com>
2024-08-18 23:48:32 +0200

    almost finish a styling part with html and start witj js

---
+++ Intro Component with signup form/intro-component-with-signup-form-master/index.js
@@ -0,0 +1,4 @@
+const name_palce = document.querySelector(".name");
+const surname_palce = document.querySelector(".surname");
+const email_palce = document.querySelector(".email");
+const password_palce = document.querySelector(".password");
